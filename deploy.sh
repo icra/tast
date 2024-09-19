3129569f7a4307c7228eb7b5a32312dc0291fb3f
Lluís Bosch <holalluis@gmail.com>
2024-09-19 16:21:12 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
-git_push_and_deploy:
-  #git: add, commit and push
-  git add *
-  git commit -am 'update'
-  git push
+#git: add, commit and push
+git add *
+git commit -am 'update'
+git push
 
-  #desplega al servidor
-  # -h  human readable format
-  # -P  mostra progrés
-  # -vv incrementa verbositat
-  # -r  actua recursivament
-  rsync -hPvr \
-    --exclude ".git" \
-    --exclude "deploy.sh" \
-    . root@icra.loading.net:/var/www/vhosts/icradev.cat/tast.icradev.cat
+#desplega al servidor
+# -h  human readable format
+# -P  mostra progrés
+# -vv incrementa verbositat
+# -r  actua recursivament
+rsync -hPvr \
+  --exclude ".git" \
+  --exclude "deploy.sh" \
+  . root@icra.loading.net:/var/www/vhosts/icradev.cat/tast.icradev.cat
