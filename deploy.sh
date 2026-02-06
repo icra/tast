9c13621a26aea71133bd44b5314aa2a808f7960b
Lluís Bosch <holalluis@gmail.com>
2026-02-06 12:52:39 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
-#check mode debug
+#!/bin/bash
+
+#check if mode debug s'ha desactivat abans de fer deploy
 cat index.html | grep "let mode_debug"
 read -p "continuar (Y/n)? " continuar
-if [ $continuar == "n" ]; then exit; fi
+if [[ $continuar == "n" ]]; then exit; fi
 
 git add *
 git commit -am 'update'
